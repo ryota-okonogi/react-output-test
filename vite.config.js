dd9df51bac3b3c7a35525a40564c7a0ef5f8eae3
nogi5000 <R5000.power@gmail.com>
2023-06-29 19:41:43 +0900

    テスト環境を構築

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/react-output-custom-test/",
+  test: {
+    global: true,
+    environment: "happy-dom",
+    setupFiles: "src/setup.js",
+  },
 });
